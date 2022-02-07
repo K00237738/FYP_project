6ed5d2cd0e33779d65df11bfe77298da509eda01
K00237738 <K00237738@student.lit.ie>
2022-02-07 12:50:11 +0000

    Update LOL_api_testing.cpp

--- LOL_api_testing/LOL_api_testing/LOL_api_testing.cpp
+++ LOL_api_testing/LOL_api_testing/LOL_api_testing.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char** argv)
 {
-	Riot::api_key = "RGAPI-33def720-b42d-4512-90c4-abcae1b53fd1";
+	Riot::api_key = "XXX";
 	Riot::region = Riot::Region::EUW;    // Note: these values default to NA
 	Riot::locale = Riot::Locale::en_US; // and en_US and do not need to be set
 
